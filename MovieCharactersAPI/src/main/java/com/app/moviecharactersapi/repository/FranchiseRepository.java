74f25d21f45420bb9e0c690d079c131f140b1edb
M A R G O <46632994+markajino@users.noreply.github.com>
2023-02-04 11:15:12 +0200

    Delete MovieCharactersAPI directory

--- MovieCharactersAPI/src/main/java/com/app/moviecharactersapi/repository/FranchiseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.moviecharactersapi.repository;
-
-import com.app.moviecharactersapi.model.Franchise;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FranchiseRepository extends JpaRepository<Franchise, Integer> {
-}
